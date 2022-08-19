377cbe950df982323cbdfc0cb89519f4f027e2a1
Gabriel de Paula <gabrieldepaula007@gmail.com>
2022-08-19 17:16:06 -0300

    Add: Testes

---
+++ NexerInsightTest/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace NexerInsightTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}

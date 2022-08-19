c840a587e3cbf37cf1228787a207f8a842284c49
Gabriel de Paula <gabrieldepaula007@gmail.com>
2022-08-19 09:55:40 -0300

    Add: AzureStorageService

---
+++ NexerInsight/Services/AzureStorageService.cs
@@ -0,0 +1,18 @@
+﻿using Azure.Storage.Blobs;
+
+namespace NexerInsight.Services
+{
+    public class AzureStorageService
+    {
+        private readonly string _connectionString;
+
+        public AzureStorageService(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("AzureStorage");
+        }
+
+        internal BlobContainerClient GetBlobContainerClient(string blobName) => new BlobServiceClient(_connectionString).GetBlobContainerClient(blobName);
+
+        internal static Stream GetStreamFromBlobClient(BlobClient blobClient) => blobClient.OpenRead();
+    }
+}

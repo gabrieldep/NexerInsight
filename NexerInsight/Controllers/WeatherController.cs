38c58d0ebe74faac5710bb1fce6448568ef9f7e2
Gabriel de Paula <gabrieldepaula007@gmail.com>
2022-08-19 09:56:35 -0300

    Delete: Useless model

--- NexerInsight/Controllers/WeatherController.cs
+++ NexerInsight/Controllers/WeatherController.cs
@@ -1,5 +1,7 @@
 using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
 using Microsoft.AspNetCore.Mvc;
+using NexerInsight.Services;
 
 namespace NexerInsight.Controllers
 {
@@ -7,11 +9,6 @@ namespace NexerInsight.Controllers
     [Route("[controller]")]
     public class WeatherController : ControllerBase
     {
-        private static readonly string[] Summaries = new[]
-        {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
-
         private readonly ILogger<WeatherController> _logger;
         private readonly IConfiguration _configuration;
 
@@ -21,24 +18,15 @@ namespace NexerInsight.Controllers
             _configuration = configuration;
         }
 
-        [HttpGet(Name = "GetWeather")]
-        public async Task<IEnumerable<Weather>> GetAsync()
+        [HttpGet(Name = "GetData")]
+        public IActionResult GetData()
         {
-            string connectionString = _configuration.GetConnectionString("AzureStorage");
-            BlobServiceClient blobServiceClient = new(connectionString);
-            BlobContainerClient containerClient = blobServiceClient.GetBlobContainerClient("iotbackend");
+            BlobContainerClient containerClient = new AzureStorageService(_configuration).GetBlobContainerClient("iotbackend");
             BlobClient blobClient = containerClient.GetBlobClient("metadata.csv");
-            string localFilePath = Path.Combine("C:\\Users\\gabri\\source\\repos\\Pessoal", "metadata.csv");
 
-            await blobClient.DownloadToAsync(localFilePath);
+            var str = AzureStorageService.GetStreamFromBlobClient(blobClient);
 
-            return Enumerable.Range(1, 5).Select(index => new Weather
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
+            return Ok();
         }
     }
 }
